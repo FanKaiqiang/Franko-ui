b9896206821caad5b31b98a2edb3262ba4a71c69
FanKaiqiang <fankaiqiang001@gmail.com>
2018-12-14 23:33:20 +0800

    还原上次的代码

---
+++ src/button-group.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="g-button-group">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  mounted(){
+    for(let node of this.$el.children){
+      let name = node.nodeName.toLowerCase()
+      if(node.nodeName.toLowerCase() !=='button'){
+        console.warn(`g-button-group 的子元素应为 g-button，但你写的是${name}`)
+      }
+    }
+  }
+};
+</script>
+<style lang="scss">
+.g-button-group{
+  display: inline-flex;
+  vertical-align: middle;
+  > .g-button{
+    border-radius: 0;
+    margin-left: -1px;
+    &:hover{
+      position: relative;
+      z-index: 1;
+    }
+    
+    &:first-child{
+      border-top-left-radius: var(--border-radius);
+      border-bottom-left-radius: var(--border-radius);
+      margin-left: 0px;
+    }
+    &:last-child{
+      border-top-right-radius: var(--border-radius);
+      border-bottom-right-radius: var(--border-radius);
+    }
+  }
+}
+</style>
---
+++ src/button.vue
@@ -0,0 +1,55 @@
+<template>
+  <button class="g-button" v-bind:class="{[`icon-${iconPosition}`]: true}" @click="$emit('click')">
+    <g-icon class="loading icon" name="loading" v-if="loading"></g-icon>
+    <g-icon class="icon" v-if="icon && !loading" :name="icon">
+    </g-icon>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
+</template>
+
+<script>
+import Icon from './icon'
+export default {
+  components:{
+    'g-icon':Icon
+  },
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: "left",
+      validator(value) {
+        return value === "left" || value === "right";
+      }
+    },
+    loading: {
+      type: Boolean,
+      default:false
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+@keyframes spin{
+  0%{  transform: rotate(0deg);  }
+  100%{    transform: rotate(360deg);  }
+}
+.g-button {
+  font-size: var(--font-size); height: var(--button-height); padding: 0 1em; border-radius: var(--border-radius);
+  border: 1px solid var(--border-color); background: var(--button-bg); display: inline-flex; 
+  justify-content: center; align-items: center; vertical-align: middle;
+  &:hover {  background-color: var(--border-color-hover); }
+  &:active {  background-color: var(--button-active-bg); }
+  &:focus {  outline: none; }
+  > .icon {  order: 1; margin-right: 0.1em;}
+  > .content {  order: 2; }
+  &.icon-right {
+    > .content {  order: 1; }
+    > .icon { order: 2; margin-right: 0; margin-left: 0.1em;}
+  }
+  .loading{  animation: spin 2s infinite linear; }
+}
+</style>
