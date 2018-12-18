04453adee113c6021884e038391b721a29a7d218
FanKaiqiang <fankaiqiang001@gmail.com>
2018-12-18 21:15:40 +0800

    完成移动端优先的响应式功能

--- src/column.vue
+++ src/column.vue
@@ -21,7 +21,19 @@ export default {
   props: {
     span: [Number, String],
     offset: [Number, String],
-    phone: {
+    ipad: {
+      type: Object,
+      validator
+    },
+    narrowPc: {
+      type: Object,
+      validator
+    },
+    pc: {
+      type: Object,
+      validator
+    },
+    widePc: {
       type: Object,
       validator
     }
@@ -31,15 +43,31 @@ export default {
       gutter: 0
     };
   },
+  methods: {
+    createClasses(obj, str = "") {
+      if (!obj) {
+        return [];
+      }
+      if (!obj.offset) {
+        obj.offset = 0;
+      }
+      return [
+        obj.span && `col-${str}-${obj.span}`,
+        `offset-${str}-${obj.offset}`
+      ];
+    }
+  },
   computed: {
     colClass() {
-      let { span, offset, phone } = this;
+      let { span, offset, ipad, narrowPc, pc, widePc } = this;
+      let createClasses = this.createClasses;
       return [
         span && `col-${span}`,
         offset && `offset-${offset}`,
-        ...(phone
-          ? [`phone-col-${phone.span}`, `phone-offset-${phone.offset}`]
-          : [])
+        ...createClasses(ipad, "ipad"),
+        ...createClasses(narrowPc, "narrow-pc"),
+        ...createClasses(pc, "pc"),
+        ...createClasses(widePc, "wide-pc")
       ];
     },
     colStyle() {
@@ -58,22 +86,31 @@ export default {
     border: 1px solid red;
   }
 
-   @mixin screen($span, $offset) {
+  @mixin screen($span, $offset) {
     @for $n from 1 through 24 {
       &.#{$span}-#{$n} {
         width: ($n / 24) * 100%;
       }
     }
-    @for $n from 1 through 24 {
+    @for $n from 0 through 24 {
       &.#{$offset}-#{$n} {
         margin-left: ($n / 24) * 100%;
       }
     }
   }
+
   @include screen(col, offset);
-  @media (max-width: 576px) {
-    @include screen(phone-col, phone-offset);
+  @media (min-width: 576px) {
+    @include screen(col-ipad, offset-ipad);
   }
-  
+  @media (min-width: 768px) {
+    @include screen(col-narrow-pc, offset-narrow-pc);
+  }
+  @media (min-width: 992px) {
+    @include screen(col-pc, offset-pc);
+  }
+  @media (min-width: 1200px) {
+    @include screen(col-wide-pc, offset-wide-pc);
+  }
 }
 </style>
