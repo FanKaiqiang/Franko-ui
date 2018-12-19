6770b3dba85e342dd3c5af95f12c05dad513488c
FanKaiqiang <fankaiqiang001@gmail.com>
2018-12-19 19:27:40 +0800

    基本完成默认布局组件

---
+++ src/footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="footer">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name:"franko-footer"
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
---
+++ src/layout.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="layout" v-bind:class="layoutClass">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name:"franko-layout",
+  data(){
+    return{
+      layoutClass:{
+        hasSider:false
+      }
+    }
+  },
+  mounted(){
+    this.$children.forEach((vm)=>{
+      if(vm.$options.name === 'franko-sider'){
+        this.layoutClass.hasSider = true
+      }
+    })
+  }
+}
+</script>
+<style lang="scss" scoped>
+.layout{
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+}
+.layout.hasSider{
+  flex-direction: row;
+}
+</style>
