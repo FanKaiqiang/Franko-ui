ae85d9a33eb753b82b6f9680c2691039ba6121ed
FanKaiqiang <fankaiqiang001@gmail.com>
2018-12-22 17:40:19 +0800

    触发 update:selected 事件发布时添加一个childVm数据

--- src/tabs.vue
+++ src/tabs.vue
@@ -36,7 +36,20 @@ export default {
     })
   },
   mounted() {
-    this.eventBus.$emit('update:selected',this.selected)
+    if(this.$children.length === 0){
+      console && console &&
+      console.warn('tabs 的自组件应该是 tabs-head 和 tabs-body，请正确设置')
+    }
+    this.$children.forEach((vm)=>{
+      if(vm.$options.name === 'franko-tabs-head'){
+        vm.$children.forEach((childVm)=>{
+          if(childVm.$options.name === 'franko-tabs-item' &&
+          childVm.name === this.selected){
+            this.eventBus.$emit('update:selected',this.selected,childVm)
+          }
+        })
+      }
+    })
   }
 }
 </script>
