b574fe8971927f3ffdaee1698ca813e65a7db995
FanKaiqiang <fankaiqiang001@gmail.com>
2018-12-25 10:21:38 +0800

    实现最简单的popover

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="popover" @click="xxx">
-    <div class="content-wrapper" v-if="visible">
+  <div class="popover" @click.stop="onClick">
+    <div class="content-wrapper" v-if="visible" @click.stop>
       <slot name="content"></slot>
     </div>
     <slot></slot>
@@ -9,29 +9,38 @@
 <script>
 export default {
   name: "franko-popover",
-  data(){
+  data() {
     return {
       visible: false
-    }
+    };
   },
-  methods:{
-    xxx(){
-      this.visible = !this.visible
+  methods: {
+    onClick() {
+      this.visible = !this.visible;
+      if (this.visible === true) {
+        this.$nextTick(() => {
+          let x = () => {
+            this.visible = false;
+            document.removeEventListener("click", x);
+          };
+          document.addEventListener("click", x);
+        });
+      }
     }
   }
 };
 </script>
 <style lang="scss" scoped>
-.popover{
+.popover {
   display: inline-block;
   vertical-align: top;
   position: relative;
-  .content-wrapper{
+  .content-wrapper {
     position: absolute;
     bottom: 100%;
     left: 0;
     border: 1px solid red;
-    box-shadow: 0 0 3px rgba(0,0,0,0.5)
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
   }
 }
 </style>
