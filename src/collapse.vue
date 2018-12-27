4a6a152f10e110d0233cf8971496077ecfe830a7
FanKaiqiang <fankaiqiang001@gmail.com>
2018-12-27 15:50:15 +0800

    父组件添加selected选项，用于存放选中的项目

--- src/collapse.vue
+++ src/collapse.vue
@@ -4,33 +4,54 @@
   </div>
 </template>
 <script>
-import Vue from 'vue'
+import Vue from "vue";
 export default {
   name: "franko-collapse",
-  props:{
-    single:{
-      type:Boolean,
-      default:false
+  props: {
+    single: {
+      type: Boolean,
+      default: false
+    },
+    selected: {
+      type: Array
     }
   },
-  data(){
-    return{
-      eventBus : new Vue()
-    }
+  data() {
+    return {
+      eventBus: new Vue(),
+      selectedArray: this.selected
+    };
   },
-  provide(){
-    if(!this.single){
-      return
-    }
-    return{
-      eventBus:this.eventBus
-    }
+  provide() {
+    return {
+      eventBus: this.eventBus
+    };
+  },
+  mounted() {
+    this.eventBus.$emit("update", this.selected);
+
+    this.eventBus.$on("add", name => {
+      if (this.single) {
+        this.selectedArray = [name];
+      } else {
+        this.selectedArray.push(name);
+      }
+      this.eventBus.$emit("update", this.selectedArray);
+      this.$emit("update:selected", this.selectedArray);
+    });
+
+    this.eventBus.$on("remove", name => {
+      let index = this.selectedArray.indexOf(name);
+      this.selectedArray.splice(index, 1);
+      this.eventBus.$emit("update", this.selectedArray);
+      this.$emit("update:selected", this.selectedArray);
+    });
   }
 };
 </script>
 <style lang="scss" scoped>
-$color:#ddd;
-$border-radius:4px;
+$color: #ddd;
+$border-radius: 4px;
 .collapse {
   border: 1px solid $color;
   border-radius: $border-radius;
